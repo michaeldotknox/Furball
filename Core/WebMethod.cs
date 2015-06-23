483f660e335f1f24e0a5402c432e98b92b0e2ff2
michaeldotknox <michael.knox@michaeldotknox.com>
2015-06-22 20:19:36 -0500

    Some changes to the WebMethod class

---
+++ Core/WebMethod.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+
+namespace Furball.Core
+{
+    public class WebMethod
+    {
+        public System.Type ReturnType { get; set; }
+        public MethodInfo MethodInfo { get; set; }
+        public List<Parameter> Parameters { get; set; }
+        public string HttpMethod { get; set; }
+        public bool HasBodyParameters { get; set; }
+    }
+}
